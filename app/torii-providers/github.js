ddbcb1f0b39727d9f04baa7bfbf6dfb763c16d86
jordan-storz <jordan.storz.1@gmail.com>
2017-03-06 04:52:13 -0700

    github provider sends info to DB server

--- app/torii-providers/github.js
+++ app/torii-providers/github.js
@@ -1,7 +1,24 @@
+import Ember from 'ember';
 import GithubOauth2Provider from 'torii/providers/github-oauth2';
 
 export default GithubOauth2Provider.extend({
+  ajax: Ember.inject.service(),
   fetch(data) {
-    return data;
-  }
+   return data;
+  },
+  open() {
+    return this._super()
+      .then(toriiData => {
+        const authCode = toriiData.authorizationCode;
+        const serverUrl = `/github_auth?code=${authCode}`;
+
+        return this.get('ajax').request(serverUrl)
+          .then(data => {
+            toriiData.accessToken = data.token;
+            console.log('received data back');
+            console.log(data);
+            return toriiData;
+          });
+      });
+  }  
 });
