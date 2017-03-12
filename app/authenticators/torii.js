03f035e8734ad9454aba0167e628cceaa6f94c85
jordan-storz <jordan.storz.1@gmail.com>
2017-03-12 12:55:48 -0600

    alter config

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
+import ENV from '../config/environment';
 
 export default ToriiAuthenticator.extend({
   torii: Ember.inject.service(),
@@ -9,7 +10,8 @@ export default ToriiAuthenticator.extend({
     return this._super(...arguments)
       .then(toriiData => {
         const authCode = toriiData.authorizationCode;
-        const serverUrl = `/github_auth?code=${authCode}`;
+        const serverUrl = ENV.APP.HOST + `/github_auth?code=${authCode}`;
+        // const serverUrl = `/github_auth?code=${authCode}`;
 
         return this.get('ajax').request(serverUrl)
           .then(response => {
