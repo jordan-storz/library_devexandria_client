b7a8ee99367d2092438ebb2b4f1e9f2e03ce62a0
jordan-storz <jordan.storz.1@gmail.com>
2017-03-12 11:27:08 -0600

    github adapter calls super

--- app/adapters/github-user.js
+++ app/adapters/github-user.js
@@ -2,5 +2,12 @@ import GitHubUserAdapter from 'ember-data-github/adapters/github-user';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default GitHubUserAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:github'
+  init() {
+    this._super(...arguments);
+  },
+  authorizer: 'authorizer:github',
+  // buildURL(model) {
+  //   console.log('BUILD URL');
+  //   console.log(model);
+  // }
 });
--- app/torii-providers/github.js
+++ app/torii-providers/github.js
@@ -6,20 +6,5 @@ export default GithubOauth2Provider.extend({
   store: Ember.inject.service(),
   fetch(data) {
     return data;
-  },
-  // open() {
-  //   return this._super()
-  //     .then(toriiData => {
-  //       const authCode = toriiData.authorizationCode;
-  //       const serverUrl = `/github_auth?code=${authCode}`;
-  //
-  //       return this.get('ajax').request(serverUrl)
-  //         .then(data => {
-  //           toriiData.accessToken = data.token;
-  //           console.log('received data back');
-  //           console.log(data);
-  //           return toriiData;
-  //         });
-  //     });
-  // }
+  }
 });
