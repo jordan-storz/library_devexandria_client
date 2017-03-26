fb0da41f76b9ea48667979be8c540bfd4cbf2265
jordan-storz <jordan.storz.1@gmail.com>
2017-03-26 17:32:45 -0600

    style glance library | change redirect env variable

--- app/components/glance-library.js
+++ app/components/glance-library.js
@@ -5,6 +5,6 @@ export default Ember.Component.extend({
   classNames: ['glance-library-component'],
   usernameExists: Ember.computed('library.user.username', function() {
     let username = this.get('library.user.username');
-    return !!username;
+    return (!!username && username.length > 1);
   })
 });
